ec75783e50f836b76d4f2d960937b5d64cf30898
mirian pacheco <mirian.alvesp@gmail.com>
2019-09-03 12:27:13 -0300

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-<<<<<<< HEAD
-  resources :members
-
-=======
   resources :members do
     resources :appointments
   end
->>>>>>> d1ce5f9358a856ff46185c688e9037619d03b385
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_205154) do
+ActiveRecord::Schema.define(version: 2019_09_03_144626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_09_02_205154) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "laboratory_vaccines", force: :cascade do |t|
