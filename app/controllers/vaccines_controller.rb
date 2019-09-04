89be9be5a1b989ed0715a53cace2d368826afc71
mirian pacheco <mirian.alvesp@gmail.com>
2019-09-04 17:24:13 -0300

    change map to vaccine-show

--- app/controllers/vaccines_controller.rb
+++ app/controllers/vaccines_controller.rb
@@ -1,2 +1,31 @@
 class VaccinesController < ApplicationController
+  before_action :set_vaccine, only: [:show]
+
+  def show
+    # .where = vaccine
+    @laboratories = Laboratory.geocoded #returns labs with coordinates
+    @markers = @laboratories.map do |laboratory|
+      if laboratory.category == "private"
+        {
+          lat: laboratory.latitude,
+          lng: laboratory.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { laboratory: laboratory }),
+          image_url: helpers.asset_url('icon_blue.png')
+        }
+      else
+        {
+          lat: laboratory.latitude,
+          lng: laboratory.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { laboratory: laboratory }),
+          image_url: helpers.asset_url('icon_red.png')
+        }
+      end
+    end
+  end
+
+  private
+
+  def set_vaccine
+    @vaccine = Vaccine.find(params[:id])
+  end
 end
