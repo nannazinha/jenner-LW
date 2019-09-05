859c85062edf70b81b5c899a0d43990900510468
Joao Thompson <joaopthompson@gmail.com>
2019-09-05 15:42:25 -0300

    finished confirmation

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,27 +3,33 @@ class AppointmentsController < ApplicationController
   before_action :set_member, only: [:new]
   before_action :set_vaccine, only: [:new]
 
-
   def show
     @appointment = Appointment.find(params[:id])
   end
 
   def new
     @appointment = Appointment.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+    @price = LaboratoryVaccine.where(laboratory: @laboratory, vaccine: @vaccine).last.price
   end
 
   def create
     @appointment = Appointment.new(appointment_params)
+    @price = LaboratoryVaccine.where(laboratory: @appointment.laboratory, vaccine: @appointment.vaccine).last.price
+    @appointment.price = @price
 
     if @appointment.save!
+      # raise
+
       redirect_to @appointment
     else
       flash[:alert] = "Something went wrong."
       render :new
     end
   end
-
-
 
   private
 
@@ -42,6 +48,4 @@ class AppointmentsController < ApplicationController
   def appointment_params
     params.require(:appointment).permit(:date, :status, :price, :laboratory_id, :member_id, :vaccine_id)
   end
-
-
 end
