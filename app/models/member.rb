ffc934c3697b9f2fb48e716fcce4632ee97905db
Joao Thompson <joaopthompson@gmail.com>
2019-09-06 16:30:15 -0300

    solved appointment dependency issue and added photo update in members

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,7 @@ class Member < ApplicationRecord
   belongs_to :user
   has_many :member_vaccines, dependent: :destroy
   has_many :vaccines, through: :member_vaccines
+  has_many :appointments, dependent: :destroy
   accepts_nested_attributes_for :member_vaccines, allow_destroy: true
   mount_uploader :photo, PhotoUploader
 end
