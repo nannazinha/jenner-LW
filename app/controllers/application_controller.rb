74cdedf78e4fd995646b98497345a9d0132f1943
natalia torres <nannazinha@gmail.com>
2019-09-06 09:26:05 -0300

    commit pro Joao Pedro trabaia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
-  def members_index
-    @members = Member.where(user: current_user)
-  end
 end
