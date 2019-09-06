4d25ffdcd331e2b192e6f6f26129ad1dd84cbfc7
natalia torres <nannazinha@gmail.com>
2019-09-06 15:37:31 -0300

    new users members validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :has_members
 
-  # @master = current_user.members.find_by(category: "0")
+  def has_members
+    if current_user
+      unless current_user.members.count.zero?
+        @master = current_user.members.find_by(category: "0")
+      else
+        redirect_to new_member_path
+      end
+    end
+  end
 end
