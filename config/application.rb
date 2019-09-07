430408724d75851704f9c04f2553163efc0b59c5
mirian pacheco <mirian.alvesp@gmail.com>
2019-09-07 15:09:05 -0300

    gem i18n and calendar translation to ptBR

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module Jenner
   class Application < Rails::Application
+    config.i18n.default_locale = :'pt-BR'
+    config.time_zone = 'Brasilia'
     config.generators do |generate|
           generate.assets false
           generate.helper false
